bcfed6e1406a3764ca11725c6659b5952bdc04f2
Brandon Pineda <brandonpineda4@gmail.com>
2022-01-20 14:17:33 -0600

    Taller 3 y reto terminado

--- Taller_3/promedio.js
+++ Taller_3/promedio.js
@@ -1,4 +1,12 @@
 
+//Sort List
+function sortList(list){
+    list = list.sort(function(accumulatedValue,newElement){
+        return accumulatedValue - newElement;
+    });
+    return list;
+}
+
 //Mean
 function calculateMean(list){
 
@@ -12,33 +20,106 @@ function calculateMean(list){
     return averageList;
 }
 
-
 // Median
-const list1=[
-    100,
-    200,
-    500,
-    400000000
-];
+function calculateMedian(list){
 
-const medianList = parseInt(list1.length /2);
-
-function even(number){
-    if(number % 2 === 0){
-        return true;
-    }else{
-        return false;
+    const medianList = parseInt(list.length /2);
+    
+    function even(number){
+        if(number % 2 === 0){
+            return true;
+        }else{
+            return false;
+        }
     }
+    
+    let median;
+    
+    if(even(list.length)){
+        const element1 = list[medianList - 1];
+        const element2 = list[medianList];
+    
+        const averageElements = calculateMean([element1, element2]);
+        median = averageElements;
+    }else{
+        median = list[medianList]; 
+    }
+    return median;
 }
 
-let median;
 
-if(even(list1.length)){
-    const element1 = list1[medianList - 1];
-    const element2 = list1[medianList];
-
-    const averageElements = calculateMean([element1, element2]);
-    median = averageElements;
-}else{
-    median = list1[medianList];
+//Mode
+function calculateMode(list){
+    const listCount={};
+    
+    list.map(
+        function(element){
+            if(listCount[element]){
+                listCount[element] += 1;
+            }else{
+                listCount[element] = 1; 
+            }
+        }
+    )
+    const listArray = Object.entries(listCount).sort(
+        function (accumulatedValue, newValue){
+            return accumulatedValue[1] - newValue[1];
+        }
+    );
+    const mode = listArray[listArray.length - 1];
+    return mode[0];
 }
+
+
+// Geometric Mean
+function calculateGeometricMean(list){
+    const multiList = list.reduce(
+        function (accumulatedValue, newElement){
+            return accumulatedValue * newElement;
+        }
+    );
+    console.log(multiList);
+    return multiList **(1/list.length); 
+}
+
+
+// HTMl
+//Aquí interactuamos con el html
+function calculateAverage(){
+    const inputList = document.getElementById("InputList");
+    const valueList = inputList.value;
+
+    let list = (valueList.split(',')).map(Number);
+    list = sortList(list); 
+
+    //Population
+    const population = list.length;
+    const populationRes = document.getElementById("populationResult");
+    populationRes.innerHTML = population; 
+
+    //Sort List 
+    const orderedList = list;
+    const sortListRes = document.getElementById("sortListResult");
+    sortListRes.innerHTML = orderedList; 
+
+    //Mean
+    const mean = calculateMean(list);
+    const meanRes = document.getElementById("meanResult");
+    meanRes.innerHTML = mean; 
+
+    //Median
+    const median = calculateMedian(list);
+    const medianRes = document.getElementById("medianResult");
+    medianRes.innerHTML = median;    
+
+    //Mode
+    const mode = calculateMode(list);
+    const modeRes = document.getElementById("modeResult");
+    modeRes.innerHTML = mode;     
+    
+    //Geometric Mean
+    const geoMean = calculateGeometricMean(list);
+    const geoMeanRes = document.getElementById("GeomeanfResult");
+    geoMeanRes.innerHTML = geoMean; 
+}
+
